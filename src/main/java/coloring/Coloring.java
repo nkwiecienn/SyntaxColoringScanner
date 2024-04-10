1e8ba2ad4f33744a2f702b452f134e6296a63d23
Oliwier <majoliwier.agh@gmail.com>
2024-04-10 22:18:27 +0200

    coloring to html code

--- src/main/java/coloring/Coloring.java
+++ src/main/java/coloring/Coloring.java
@@ -4,12 +4,16 @@ import scanner.Scanner;
 import token.Token;
 import token.TokenType;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
+import java.io.BufferedWriter;
 public class Coloring {
     List<Token> tokens;
     HashMap<TokenType, String> coloringMap = new HashMap<>();
+    HashMap<TokenType, String> htmlColorMap = new HashMap<>();
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_GRAY = "\u001b[38;2;38;70;83m";
     public static final String ANSI_RED_BACKGROUND = "\u001b[48;2;214;40;40m";
@@ -24,6 +28,7 @@ public class Coloring {
         Scanner scanner = new Scanner(inputFile);
         tokens = scanner.getAllTokens();
         this.makeColoringMap();
+        this.makeHtmlColorMap();
     }
 
     private void makeColoringMap() {
@@ -40,8 +45,40 @@ public class Coloring {
 
     }
 
+    private void makeHtmlColorMap() {
+        htmlColorMap.put(TokenType.RESERVED, "#F4A261");
+        htmlColorMap.put(TokenType.BUILT_IN_TYPES, "#2A9D8F");
+        htmlColorMap.put(TokenType.SYMBOL, "#FFFFFF");
+        htmlColorMap.put(TokenType.STRING, "#E9C46A");
+        htmlColorMap.put(TokenType.NUMBER, "#00B4D8");
+        htmlColorMap.put(TokenType.QUOTATION, "#E76F51");
+        htmlColorMap.put(TokenType.COMMENT, "#264653");
+        htmlColorMap.put(TokenType.WHITE_SPACE, "");
+        htmlColorMap.put(TokenType.EOF, "");
+        htmlColorMap.put(TokenType.ERROR, "#D62828");
+    }
+
     public void print() {
-        for(Token t : tokens)
+        for (Token t : tokens)
             System.out.print(coloringMap.get(t.getType()) + t.getToken() + ANSI_RESET);
+    }
+
+    public void printToHtml(String outputFile) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+            writer.write("<!DOCTYPE html><html><head><title>Colored Code</title>");
+            writer.write("<style>body { background-color: #000; color: #fff; } pre { white-space: pre-wrap; word-wrap: break-word; } span { color: inherit; }</style>");
+            writer.write("</head><body><pre>");
+
+            for (Token t : tokens) {
+                String color = htmlColorMap.get(t.getType());
+                String token = t.getToken();
+                if (!color.isEmpty()) {
+                    writer.write(String.format("<span style=\"color:%s;\">%s</span>", color, token));
+                } else {
+                    writer.write(token);
+                }
+            }
+            writer.write("</pre></body></html>");
+        }
     }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,9 +2,15 @@ package main;
 
 import coloring.Coloring;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Coloring coloring = new Coloring("src/main/java/main/QuickSort.java");
         coloring.print();
+        File f = new File(".\\Output");
+        f.mkdir();
+        coloring.printToHtml("Output/Colored Code.html");
     }
 }
+
